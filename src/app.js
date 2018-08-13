02d5296de660e20361edd602631c3c7ed4da66e8
David A Pears <davidapears@gmail.com>
2018-08-13 17:01:01 +0100

    Select beer by name dropdown box added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const Beers = require('./models/beers.js');
 const BeerListView = require('./views/beer_list_view.js');
 const BeerFormView = require('./views/beer_form_view.js');
+const SelectBeerView = require('./views/select_beer_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
 
+// Should create a drop-down box with all beer names:
+  const selectElement = document.querySelector('select#beers');
+  const selectView = new SelectBeerView(selectElement);
+  selectView.bindEvents();
+
+// The beow returns a searchbox, where beer can be searched by name
   const beerForm = document.querySelector('form')
   const beerFormView = new BeerFormView(beerForm);
   beerFormView.bindEvents();
@@ -16,7 +23,5 @@ document.addEventListener('DOMContentLoaded', () => {
   const beers = new Beers;
   beers.getData();
   beers.bindEvents();
-
-
 
 })
---
+++ src/views/select_beer_view.js
@@ -0,0 +1,32 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const SelectBeerView = function (selectElement) {
+  this.element = selectElement;
+};
+
+SelectBeerView.prototype.bindEvents = function () {
+  PubSub.subscribe('Beers:beers-ready', (evt) => {
+    this.populate(evt.detail)
+  });
+
+  this.element.addEventListener('change', (evt) => {
+    const selectedIndex = evt.target.value;
+    PubSub.publish('SelectView:change', selectedIndex);
+  })
+};
+
+SelectBeerView.prototype.populate = function (beers) {
+  beers.forEach((beer, index) => {
+    const beerOption = this.createOption(beer.name, index);
+    this.element.appendChild(beerOption);
+  });
+};
+
+SelectBeerView.prototype.createOption = function (name, index) {
+  const option = document.createElement('option');
+  option.textContent = name;
+  option.value = index;
+  return option;
+};
+
+module.exports = SelectBeerView;
